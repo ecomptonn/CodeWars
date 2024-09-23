23af4d6d86d4459139902edeaf9552414d7747f3
ecomptonn <162512945+ecomptonn@users.noreply.github.com>
2024-09-22 19:14:25 -0700

    Solution

---
+++ 8kyu-i-love-you-a-little-a-lot-passionately-not-at-all.js
@@ -0,0 +1,12 @@
+function howMuchILoveYou(nbPetals) {
+    const phrases = {
+        1: "I love you",
+        2: "a little",
+        3: "a lot",
+        4: "passionately",
+        5: "madly",
+        6: "not at all",
+    };
+
+    return phrases[nbPetals % 6 === 0 ? 6 : nbPetals % 6];
+}
