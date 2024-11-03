56652b5e95db4d99bdb7422523b97ec88cab916b
ecomptonn <162512945+ecomptonn@users.noreply.github.com>
2024-11-02 18:33:58 -0700

    Solution

---
+++ 8kyu-filter-out-the-geese.js
@@ -0,0 +1,11 @@
+function gooseFilter(birds) {
+    var geese = [
+        "African",
+        "Roman Tufted",
+        "Toulouse",
+        "Pilgrim",
+        "Steinbacher",
+    ];
+
+    return birds.filter((bird) => !geese.includes(bird));
+}
