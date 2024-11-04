9aa1b4b7f41d335be732fd519450e978a07976a7
ecomptonn <162512945+ecomptonn@users.noreply.github.com>
2024-11-03 17:39:08 -0700

    Solution

---
+++ 8kyu-is-the-string-uppercase.js
@@ -0,0 +1,9 @@
+String.prototype.isUpperCase = function () {
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] !== this[i].toUpperCase()) {
+            return false;
+        }
+    }
+
+    return true;
+};
