699d14a836b2b39f1a354cc560f4577bce31dfc3
ecomptonn <162512945+ecomptonn@users.noreply.github.com>
2024-11-04 18:09:54 -0700

    Solution

---
+++ 8kyu-training-js-#4-array.js
@@ -0,0 +1,18 @@
+function getLength(arr) {
+    return arr.length;
+}
+function getFirst(arr) {
+    return arr.shift();
+}
+function getLast(arr) {
+    return arr.pop();
+}
+function pushElement(arr) {
+    var el = 1;
+    arr.push(el);
+    return arr;
+}
+function popElement(arr) {
+    arr.pop();
+    return arr;
+}
